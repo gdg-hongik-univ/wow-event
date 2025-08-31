03359cb85086a1b5c9e4f7c7adc97859543c4db8
chocoboy08 <chocoboy0508@gmail.com>
2025-08-31 16:42:06 +0900

    feat: Flex 컴포넌트 추가

---
+++ src/components/Flex.tsx
@@ -0,0 +1,74 @@
+import type { CSSProperties, PropsWithChildren } from "react";
+import type { color, space } from "wowds-tokens";
+import { color as wowColor, space as wowSpace } from "wowds-tokens";
+type spaceType = keyof typeof space | number;
+type colorType = keyof typeof color;
+type CSSFlexDirection = "row" | "row-reverse" | "column" | "column-reverse";
+
+type CSSJustifyContent =
+  | "flex-start"
+  | "flex-end"
+  | "center"
+  | "space-between"
+  | "space-around"
+  | "space-evenly"
+  | "start"
+  | "end";
+
+type CSSAlignItems =
+  | "stretch"
+  | "flex-start"
+  | "flex-end"
+  | "center"
+  | "baseline"
+  | "start"
+  | "end";
+interface FlexProps extends PropsWithChildren {
+  direction?: CSSFlexDirection;
+  justify?: CSSJustifyContent;
+  align?: CSSAlignItems;
+  margin?: spaceType;
+  padding?: spaceType;
+  gap?: spaceType;
+  bgColor?: colorType;
+  radius?: spaceType;
+  width?: number;
+  style?: CSSProperties;
+}
+
+const Flex = ({
+  children,
+  direction = "row",
+  justify = "flex-start",
+  align = "stretch",
+  margin = 0,
+  padding = 0,
+  gap = 0,
+  bgColor = "backgroundNormal",
+  radius = 0,
+  width,
+  style,
+}: FlexProps) => {
+  return (
+    <div
+      className="flex box-border"
+      style={{
+        flexDirection: direction,
+        justifyContent: justify,
+        alignItems: align,
+        gap: typeof gap === "number" ? gap : wowSpace[gap],
+        margin: typeof margin === "number" ? margin : wowSpace[margin],
+        padding: typeof padding === "number" ? padding : wowSpace[padding],
+        borderRadius: typeof radius === "number" ? radius : wowSpace[radius],
+        width: width || "100%",
+        backgroundColor:
+          typeof wowColor[bgColor] === "string" ? wowColor[bgColor] : "none",
+        ...style,
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Flex;
