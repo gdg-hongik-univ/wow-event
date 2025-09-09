981ca6904a8a094bc98f5c2b8a278d725fccced6
chocoboy08 <chocoboy0508@gmail.com>
2025-09-09 17:56:40 +0900

    fix: 404 페이지 수정

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,18 +1,10 @@
-import { useNavigate } from "react-router";
-import Button from "wowds-ui/Button";
 import Flex from "../components/Flex";
 import Text from "../components/Text";
-import { RoutePath } from "../routes/routePath";
 
 export default function NotFoundErrorPage() {
-  const navigate = useNavigate();
-
-  const handleClickNavigateToHome = () => navigate(RoutePath.Index);
-
   return (
-    <Flex direction="column">
+    <Flex direction="column" justify="center" align="center">
       <Text>페이지를 찾을 수 없습니다.</Text>
-      <Button onClick={handleClickNavigateToHome}>홈으로 돌아가기</Button>
     </Flex>
   );
 }
