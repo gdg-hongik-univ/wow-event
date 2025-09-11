c818b325b538a776fee19768195159da688ebb36
chocoboy08 <chocoboy0508@gmail.com>
2025-09-11 16:11:25 +0900

    refactor: 정보 입력, 폼 입력 페이지 분리

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,18 +1,23 @@
 import { createBrowserRouter, type RouteObject } from "react-router";
-import Layout from "../components/Layout";
+import FormLayout from "../components/FormLayout";
+import AuthServerRedirectPage from "../pages/AuthServerRedirectPage";
+import BasicInfoPage from "../pages/BasicInfoPage";
+import FormPage from "../pages/FormPage";
 import NotFoundErrorPage from "../pages/NotFoundPage";
 import SignInPage from "../pages/SignInPage";
 import { RoutePath } from "./routePath";
 
 const routes: RouteObject[] = [
   {
-    element: <Layout />,
+    element: <FormLayout />,
     children: [
-      { path: RoutePath.Index, element: <></> },
+      { path: RoutePath.Form, element: <FormPage /> },
+      { path: RoutePath.Info, element: <BasicInfoPage /> },
       { path: RoutePath.SignIn, element: <SignInPage /> },
     ],
   },
   { path: "*", element: <NotFoundErrorPage /> },
+  { path: RoutePath.AuthServerRedirect, element: <AuthServerRedirectPage /> },
 ];
 
 const Router = createBrowserRouter(routes);
