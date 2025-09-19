d50364f23dc239da5811e3eb9ba22e8398d72fa8
chocoboy08 <chocoboy0508@gmail.com>
2025-09-19 13:32:20 +0900

    fix: 코드 리뷰 반영

--- src/apis/fetcher.ts
+++ src/apis/fetcher.ts
@@ -1,4 +1,4 @@
 import { apiClient } from ".";
 
-export const fetcher = (url: string) =>
-  apiClient.get(url).then((res) => res.data);
+export const fetcher = (url: string, { signal }: { signal: AbortSignal }) =>
+  apiClient.get(url, { signal }).then((res) => res.data);
