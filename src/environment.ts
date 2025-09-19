2e5b2afe453b6531f5f2dd48f586807ce82c5e57
chocoboy08 <chocoboy0508@gmail.com>
2025-09-19 22:55:27 +0900

    fix: base url 연결 방식 수정

--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,1 @@
-export const BASE_URL =
-  import.meta.env.MODE === "production"
-    ? import.meta.env.VITE_DEV_BASE_URL.VITE_PROD_BASE_URL
-    : import.meta.env.VITE_DEV_BASE_URL;
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
