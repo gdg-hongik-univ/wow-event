2fa000b72efb4fd10a3391a61645cfd2bd0ccbb1
chocoboy08 <chocoboy0508@gmail.com>
2025-10-16 21:24:16 +0900

    feat: 에러 타입 정의

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,9 +1,11 @@
+import type { AxiosError } from "axios";
 import useSWR from "swr";
 import { fetcher } from "../apis/fetcher";
+import type { ErrorDataType } from "../types/error";
 import type { EventDtoType } from "../types/event";
 
 export const useEventFetcher = (eventId: string | undefined) => {
-  const { data, error } = useSWR<EventDtoType>(
+  const { data, error } = useSWR<EventDtoType, AxiosError<ErrorDataType>>(
     eventId ? `/participant/events/${eventId}` : null,
     fetcher
   );
