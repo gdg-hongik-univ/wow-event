413dc4d6d985523300b0ab42660851dda2ce1aa3
chocoboy08 <chocoboy0508@gmail.com>
2025-10-16 21:32:05 +0900

    feat: 모바일 뷰 추가 대응

--- src/components/FormDescription.tsx
+++ src/components/FormDescription.tsx
@@ -1,3 +1,4 @@
+import { useResponsive } from "../hooks/useResponsive";
 import Text from "./base/Text";
 
 interface FormDescriptionProp {
@@ -11,6 +12,8 @@ const FormDescription = ({
   venue,
   applicationDescription,
 }: FormDescriptionProp) => {
+  const { isMobile } = useResponsive();
+
   const startDate = new Date(startAt);
 
   const showDate = `${startDate.getFullYear()}년 ${
@@ -18,7 +21,7 @@ const FormDescription = ({
   }월 ${startDate.getDate()}일 ${startDate.getHours()}시 ${startDate.getMinutes()}분`;
 
   return (
-    <Text style={{ width: "100%" }}>
+    <Text style={{ width: "100%" }} typo={isMobile ? "body2" : "body1"}>
       {!isNaN(startDate.getTime()) && `행사 일시: ${showDate}`}
       <br />
       {venue && `행사 장소: ${venue}`}
