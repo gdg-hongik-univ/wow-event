959df62a7e8173457a06ed950cdad3dce5476ed4
leejuhi <anim0203@naver.com>
2026-01-11 13:46:38 +0900

    feat: 기본 글자 md로 보이기

--- src/components/FormDescription.tsx
+++ src/components/FormDescription.tsx
@@ -1,5 +1,7 @@
 import { useResponsive } from "../hooks/useResponsive";
+import Flex from "./base/Flex";
 import Text from "./base/Text";
+import ReactMarkdown from "react-markdown";
 
 interface FormDescriptionProp {
   venue: string;
@@ -20,18 +22,39 @@ const FormDescription = ({
     startDate.getMonth() + 1
   }월 ${startDate.getDate()}일 ${startDate.getHours()}시 ${startDate.getMinutes()}분`;
 
+  console.log(description);
   return (
-    <Text
-      style={{ width: "100%", whiteSpace: "break-spaces" }}
-      typo={isMobile ? "body2" : "body1"}
-    >
-      {!isNaN(startDate.getTime()) && `행사 일시: ${showDate}`}
-      <br />
-      {venue && `행사 장소: ${venue}`}
-      <br />
-      <br />
-      {description}
-    </Text>
+    <>
+      <Flex direction="column" gap={16} width="100%">
+        <Text
+          style={{
+            width: "100%",
+            whiteSpace: "break-spaces",
+            backgroundColor: "#FFFFFF",
+            padding: isMobile ? 16 : 24,
+            borderRadius: 8,
+            fontWeight: "700",
+          }}
+          typo={isMobile ? "body2" : "body1"}
+        >
+          {!isNaN(startDate.getTime()) && `행사 일시: ${showDate}`}
+          <br />
+          {venue && `행사 장소: ${venue}`}
+        </Text>
+        <Text
+          style={{
+            width: "100%",
+            whiteSpace: "break-spaces",
+            backgroundColor: "#FFFFFF",
+            padding: isMobile ? 16 : 24,
+            borderRadius: 8,
+          }}
+          typo={isMobile ? "body2" : "body1"}
+        >
+          <ReactMarkdown>{description}</ReactMarkdown>
+        </Text>
+      </Flex>
+    </>
   );
 };
 
